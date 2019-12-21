ce46686c88c901f14965b712c6704830ef39121d
KOTlK <55581536+KOTlK@users.noreply.github.com>
2019-12-21 16:33:38 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ShutOff_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.userText = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.Seconds = new System.Windows.Forms.RadioButton();
+            this.Hours = new System.Windows.Forms.RadioButton();
+            this.Minutes = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // userText
+            // 
+            this.userText.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.userText.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userText.Location = new System.Drawing.Point(89, 76);
+            this.userText.Multiline = true;
+            this.userText.Name = "userText";
+            this.userText.Size = new System.Drawing.Size(160, 38);
+            this.userText.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(89, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Set your time here";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.Location = new System.Drawing.Point(89, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Turn OFF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Cancel.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Cancel.Location = new System.Drawing.Point(89, 341);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(160, 47);
+            this.Cancel.TabIndex = 3;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // Seconds
+            // 
+            this.Seconds.AutoSize = true;
+            this.Seconds.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Seconds.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Seconds.Location = new System.Drawing.Point(89, 222);
+            this.Seconds.Name = "Seconds";
+            this.Seconds.Size = new System.Drawing.Size(81, 23);
+            this.Seconds.TabIndex = 4;
+            this.Seconds.TabStop = true;
+            this.Seconds.Text = "Seconds";
+            this.Seconds.UseVisualStyleBackColor = true;
+            // 
+            // Hours
+            // 
+            this.Hours.AutoSize = true;
+            this.Hours.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Hours.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Hours.Location = new System.Drawing.Point(89, 164);
+            this.Hours.Name = "Hours";
+            this.Hours.Size = new System.Drawing.Size(68, 23);
+            this.Hours.TabIndex = 5;
+            this.Hours.TabStop = true;
+            this.Hours.Text = "Hours";
+            this.Hours.UseVisualStyleBackColor = true;
+            // 
+            // Minutes
+            // 
+            this.Minutes.AutoSize = true;
+            this.Minutes.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Minutes.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Minutes.Location = new System.Drawing.Point(89, 193);
+            this.Minutes.Name = "Minutes";
+            this.Minutes.Size = new System.Drawing.Size(81, 23);
+            this.Minutes.TabIndex = 6;
+            this.Minutes.TabStop = true;
+            this.Minutes.Text = "Minutes";
+            this.Minutes.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(354, 450);
+            this.Controls.Add(this.Minutes);
+            this.Controls.Add(this.Hours);
+            this.Controls.Add(this.Seconds);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.userText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(370, 489);
+            this.MinimumSize = new System.Drawing.Size(370, 489);
+            this.Name = "Form1";
+            this.Text = "ShutOff";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userText;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.RadioButton Seconds;
+        private System.Windows.Forms.RadioButton Hours;
+        private System.Windows.Forms.RadioButton Minutes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutOff_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string timer = userText.Text;
+            int timetilloff = Convert.ToInt32(timer);
+            if (timer == "")
+            {
+                timetilloff = Convert.ToInt32(timer);
+            }
+            if (Hours.Checked)
+            {
+                timetilloff = Convert.ToInt32(timer) * 3600;
+            } else if (Minutes.Checked)
+            {
+                timetilloff = Convert.ToInt32(timer) * 60;
+            } else if (Seconds.Checked)
+            {
+                timetilloff = Convert.ToInt32(timer);
+            }
+            Process.Start("cmd", "/c shutdown -s -f -t " + timetilloff);
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd", "/c shutdown /a");
+        }
+    }
+}
